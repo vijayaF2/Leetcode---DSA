5f4fb6c7e73e79135e75bac04d3e6a1955e2fe7f
CH NAGA VIJAYA LAKSHMI <119723559+vijayaF2@users.noreply.github.com>
2025-06-03 14:43:46 +0530

    Time: 7 ms (51.29%), Space: 20.2 MB (25.97%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(numCourses,0);
+        unordered_map<int,vector<int>>m;
+        for(int i=0;i<prerequisites.size();i++){
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            indegree[u]++;
+            m[v].push_back(u);
+        }
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<indegree.size();i++){
+              if(indegree[i]==0) {
+              q.push(i);
+              ans.push_back(i);
+              }
+        }
+        while(!q.empty())
+        {
+            int x=q.front();
+            q.pop();
+            for(int i=0;i<m[x].size();i++)
+            {
+                indegree[m[x][i]]--;
+                if(indegree[m[x][i]]==0) {
+                    q.push(m[x][i]);
+                    ans.push_back(m[x][i]);
+                }
+            }
+        }
+        if(ans.size()==numCourses) return true;
+        else{
+            return false;
+        }
+
+        
+    }
+};
