faeec37ae1fa8703cfc4ae1c9238b48c078467b5
CH NAGA VIJAYA LAKSHMI <119723559+vijayaF2@users.noreply.github.com>
2025-07-06 18:18:30 +0530

    Time: 0 ms (100%), Space: 30.3 MB (24.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k=k%nums.size();
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-       
-        
+       ///// without using extra space:
+       /// if k exceeds the size of the array 
+    //    k=k%nums.size();
+    //    reverse(nums.begin(),nums.end());
+    //    reverse(nums.begin(),nums.begin()+k);
+    //    reverse(nums.begin()+k,nums.end());
+
+
+        /// with using extra space:
+        vector<int>ans(nums.begin(),nums.end());
+        for(int i=0;i<ans.size();i++)
+        {
+            nums[(i+k)%nums.size()]=ans[i];
+        } 
     }
 };
