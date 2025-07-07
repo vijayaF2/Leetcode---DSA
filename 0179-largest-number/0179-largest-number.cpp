a1e9b341572a0fad3c79f947a793ce2f31f0f938
CH NAGA VIJAYA LAKSHMI <119723559+vijayaF2@users.noreply.github.com>
2025-07-07 09:34:37 +0530

    Time: 7 ms (26.78%), Space: 17.2 MB (18.36%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool cmp(string &a,string &b){
+            return a+b > b+a;
+         }
+    string largestNumber(vector<int>& nums) {
+        vector<string>v;
+        for(int i=0;i<nums.size();i++)
+        {
+            v.push_back(to_string(nums[i]));
+        }
+        sort(v.begin(),v.end(),cmp);
+        if(v[0]=="0") return "0";
+        string ans="";
+        for(int i=0;i<v.size();i++)
+        {
+            ans+=v[i];
+        }
+        return ans;
+        
+    }
+};
