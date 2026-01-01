514f1924244951625faf240e317a3f656756d9a8
CH NAGA VIJAYA LAKSHMI <119723559+vijayaF2@users.noreply.github.com>
2026-01-01 10:52:41 +0530

    Time: 0 ms (100%), Space: 11.5 MB (50.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int>ans;
-        int carry=0;
-        int add=digits[digits.size()-1]+1;
-        if(add<10) ans.push_back(add);
-        else{
-            ans.push_back(add%10);
-            carry=add/10;
-        }
-        for(int i=digits.size()-2;i>=0;i--)
-        {
-            if(digits[i]+carry <10){
-             ans.push_back(digits[i]+carry);
-             carry=0;
+        // vector<int>ans;
+        // int carry=0;
+        // int add=digits[digits.size()-1]+1;
+        // if(add<10) ans.push_back(add);
+        // else{
+        //     ans.push_back(add%10);
+        //     carry=add/10;
+        // }
+        // for(int i=digits.size()-2;i>=0;i--)
+        // {
+        //     if(digits[i]+carry <10){
+        //      ans.push_back(digits[i]+carry);
+        //      carry=0;
+        //     }
+        //     else{
+        //         ans.push_back((digits[i]+carry)%10);
+        //         carry=(digits[i]+carry)/10;
+        //     }
+        // }
+        // if(carry>0) ans.push_back(carry);
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
             }
-            else{
-                ans.push_back((digits[i]+carry)%10);
-                carry=(digits[i]+carry)/10;
-            }
-        }
-        if(carry>0) ans.push_back(carry);
-        reverse(ans.begin(),ans.end());
-        return ans;
-        
+            digits[i]=0;
+        }        
+        digits.insert(digits.begin(),1);
+        return digits;
     }
 };
