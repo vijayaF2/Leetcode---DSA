e77f8e0c19d28b6b3dbaeb1f313254714c532108
CH NAGA VIJAYA LAKSHMI <119723559+vijayaF2@users.noreply.github.com>
2026-01-20 11:41:31 +0530

    Time: 4 ms (23.67%), Space: 25.2 MB (55.03%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int>ans(nums.size(),-1);
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=1;j<=nums[i];j++)
+            {
+                if((j|j+1)==nums[i]) {
+                    ans[i]=j;
+                break;
+                }
+            }
+        }
+        return ans;
+    }
+};
