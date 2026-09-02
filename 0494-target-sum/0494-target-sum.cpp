80e299c9e3581cf81643834eac5fe8ed35eafb13
CH NAGA VIJAYA LAKSHMI <119723559+vijayaF2@users.noreply.github.com>
2026-09-02 18:31:08 +0530

    Time: 13 ms (52.97%), Space: 25.7 MB (27.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-   int fun(int i,int sum, int target,vector<int>&nums)
+   int fun(int i,int sum, int target,int offset,vector<int>&nums,vector<vector<int>>&dp)
    {
     if(i==nums.size() && sum==target) return 1;
     if(i==nums.size()) return 0;
-    int left=fun(i+1,sum-nums[i],target,nums);
-    int right=fun(i+1,sum+nums[i],target,nums);
-    return left+right;
+    if(dp[i][sum+offset]!=-1) return dp[i][sum+offset];
+    int left=fun(i+1,sum-nums[i],target,offset,nums,dp);
+    int right=fun(i+1,sum+nums[i],target,offset,nums,dp);
+    return dp[i][sum+offset]=left+right;
    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int i=0,sum=0;
-        return fun(i,sum,target,nums);
+        int i=0,sum=0,total=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            total+=nums[i];
+        }
+        int offset=total;
+        vector<vector<int>>dp(nums.size(),vector<int>(2*total+1,-1));
+        return fun(i,sum,target,offset,nums,dp);
         
     }
 };
